1b5b53d78d74f77e37070fe8be7e4ad58b14fb84
stoneGal <bashorunbethel99@gmail.com>
2020-03-02 14:58:24 +0100

    first tp commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// let bill, tip, total, calc;
+
+// function Tipcalculate(){
+// bill = prompt('kindly enter the amount of your bill in naira');
+// alert('you input a bill of  ' + bill + '₦');
+// tip = prompt('enter the amount of your tip in percentage');
+// alert('you input a tip of  ' + tip + '%');
+//  calc = (Math.floor(tip/100)) * bill;
+//  total = (calc + bill);
+// alert('your tip is ' + total+'₦');
+
+$(document).ready(function() {
+  $('#formButton').click(function() {
+    $('#tipcalc').toggle();
+  });
+});
+
+function calculateTip() {
+  //getElementById() method returns the element value with specified ID
+
+  let billAmt = document.getElementById('billAmt').value;
+  let servicequality = document.getElementById('servicequality').value;
+  let tipTotal = document.getElementById('tipTotal').value;
+  let billTotal = document.getElementById('billTotal').value;
+  let peopleshare = document.getElementById('peopleshare').value;
+  let each = document.getElementById('each').value;
+
+  let total = billAmt * (servicequality / 100);
+  let Total = Math.floor(parseFloat(billAmt) + parseFloat(total));
+  let individual = Total / peopleshare;
+
+  //Display Total
+  document.getElementById('tipTotal').innerHTML = total + '₦';
+  document.getElementById('billTotal').innerHTML = Total + '₦';
+  document.getElementById('each').innerHTML =
+    individual + '₦  ' + '    per person';
+}
+
+//Click to call function
+document.getElementById('calculate').onclick = function() {
+  calculateTip();
+};
